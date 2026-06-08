6a101e28d0d620e9340127203985b2f9474e2303
oilerart <euler@caseproof.com>
2026-06-08 14:26:53 -0300

    feat: add plugin skeleton with constants and activation hooks

---
+++ itc.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Plugin Name: itc - is this cached?
+ * Description: Detects caching layers for any WordPress content.
+ * Version: 0.1.0
+ * Author: oilerart
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'ITC_VERSION', '0.1.0');
+define( 'ITC_DIR', plugin_dir_path( __FILE__) );
+define( 'ITC_URL', plugin_dir_url( __FILE__ ) );
+
+register_activation_hook( __FILE__, 'itc_activate' );
+register_deactivation_hook( __FILE__, 'itc_deactivate' );
+
+function itc_activate() {
+    
+}
+
+function itc_deactivate() {
+
+}
+
+add_action( 'plugins_loaded', function() {
+
+});
