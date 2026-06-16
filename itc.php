2eceaf023192db28150baed8a65c1ea24aeee487
oilerart <euler@caseproof.com>
2026-06-16 15:32:11 -0300

    feat: read expires, etag, last-modified and age headers

--- itc.php
+++ itc.php
@@ -50,8 +50,12 @@ class ITC {
     public function render_page() {        
         
         $scanned_url = '';
-        $cache_control = '';
         $error = '';
+        $cache_control = '';
+        $expires = '';
+        $etag = '';
+        $last_modified = '';
+        $age = '';
 
         if ( isset( $_POST['itc_nonce'] ) && wp_verify_nonce( $_POST['itc_nonce'], 'itc_scan' ) ) {
             $scanned_url = esc_url_raw( $_POST['itc_url'] ?? '' );
@@ -64,10 +68,34 @@ class ITC {
                 } else {
                     $headers = wp_remote_retrieve_headers( $response );
                     $cache_control = $headers['cache-control'] ?? '';
+                    $expires = $headers['expires'] ?? '';
+                    $etag = $headers['etag'] ?? '';
+                    $last_modified = $headers['last-modified'] ?? '';
+                    $age = $headers['age'] ?? '';
                 }
             }
 
         }
+
+        ?>
+
+        <div class="wrap">
+            <h1>Is this cached?</h1>
+            <form method="post">
+                <?php wp_nonce_field( 'itc_scan', 'itc_nonce' ); ?>
+                <input type="url" name="itc_url" placeholder="https://example.com" class="regular-text">
+                <input type="submit" class="button button-primary" value="Scan">
+            </form>
+
+            <?php if ( $error ) : ?>
+                <p>Error: <?php echo esc_html( $error ); ?></p>
+            <?php elseif ( $scanned_url ) : ?>
+                <p>Scanning: <?php echo esc_url( $scanned_url ); ?> </p>
+                <p>Cache control: <?php echo esc_html( $cache_control ); ?></p>
+            <?php endif; ?>
+        </div>
+
+        <?php
 
     }
 }
